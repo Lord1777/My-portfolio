b70f40ce11b683b59d93df6c5a9bde0ef0710b78
Lord1777 <canegrn@misena.edu.co>
2024-04-30 09:16:36 -0500

    Acomodando las columnas, ajustando el estilo de las barras de progreso :)

--- My portfolio/script.js
+++ My portfolio/script.js
@@ -1,3 +1,8 @@
 document.getElementById("homeLink").addEventListener("click", function() {
   window.location.href = "#home";
 });
+
+document.querySelector('a[href="#skills"]').addEventListener('click', function(event) {
+  event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+  document.getElementById('skills').scrollIntoView({ behavior: 'smooth' }); // Desplaza suavemente hacia la sección de habilidades
+});

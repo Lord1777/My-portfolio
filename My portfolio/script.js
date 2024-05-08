d5a5bfc9defe0de194000c08e131cf54e2d11d66
Lord1777 <canegrn@misena.edu.co>
2024-05-08 16:37:12 -0500

    Avanzando poco a poco en el responsive :)

--- My portfolio/script.js
+++ My portfolio/script.js
@@ -1,56 +1,21 @@
-// Función para manejar el comportamiento de desplazamiento suave al hacer clic en el enlace del Navbar
-function smoothScrollTo(target) {
-  window.scrollTo({
-    top: target.offsetTop,
-    behavior: "smooth"
-  });
-}
 
-// Obtener elementos del DOM una vez al cargar la página
-document.addEventListener("DOMContentLoaded", function() {
-  var navbar = document.querySelector(".Navbar");
-  var mainContainer = document.querySelector('.main_container');
-  var check = document.getElementById('check');
+var header = document.querySelector(".Header");
 
-  // Obtener la posición de inicio del Navbar
-  var sticky = navbar.offsetTop;
+var sticky = header.offsetTop;
 
-  // Función para manejar el estado fijo del Navbar
-  function handleFixedNavbar() {
+var mainContainer = document.querySelector('.main_container');
+
+window.addEventListener("scroll", function() {
+    
     if (window.pageYOffset >= sticky) {
-      navbar.classList.add("fixed");
-      mainContainer.style.marginTop = navbar.offsetHeight + 'px';
+        
+        header.classList.add("fixed");
+        
+        mainContainer.style.marginTop = header.offsetHeight + 'px';
     } else {
-      navbar.classList.remove("fixed");
-      mainContainer.style.marginTop = '0';
+       
+        header.classList.remove("fixed");
+        
+        mainContainer.style.marginTop = '0';
     }
-  }
-
-  // Evento de desplazamiento para cambiar el estado del Navbar
-  window.addEventListener("scroll", handleFixedNavbar);
-
-  // Evento de clic en el enlace del Navbar
-  document.getElementById("navbar").addEventListener("click", function(event) {
-    var target = document.querySelector(event.target.getAttribute('href'));
-    smoothScrollTo(target);
-    mainContainer.style.marginTop = navbar.offsetHeight + 'px';
-    check.checked = false; // Cerrar el menú al hacer clic en el enlace
-  });
-
-  // Evento de clic en el icono del menú
-  document.querySelector('.menu-icon').addEventListener('click', function(event) {
-    event.preventDefault();
-    document.querySelector('ul').classList.toggle('active');
-  });
-
-  // Cerrar el menú cuando se hace clic fuera del menú
-  window.addEventListener('click', function(event) {
-    var menuIcon = document.getElementById('menu-icon');
-    if (!event.target.matches('.icons') && !event.target.matches('#check') && !event.target.matches('#menu-icon')) {
-      check.checked = false;
-    }
-  });
-
-  // Llamar a la función para inicializar el estado del Navbar
-  handleFixedNavbar();
 });

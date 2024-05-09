c136c2fccab74125ba5ce02fe6e03c9e4d850cfe
Lord1777 <canegrn@misena.edu.co>
2024-05-09 09:50:24 -0500

    Terminando el responsive de las cartas de habilidades :)

--- My portfolio/script.js
+++ My portfolio/script.js
@@ -1,21 +1,15 @@
-
 var header = document.querySelector(".Header");
 
 var sticky = header.offsetTop;
 
-var mainContainer = document.querySelector('.main_container');
+var mainContainer = document.querySelector(".main_container");
 
-window.addEventListener("scroll", function() {
-    
-    if (window.pageYOffset >= sticky) {
-        
-        header.classList.add("fixed");
-        
-        mainContainer.style.marginTop = header.offsetHeight + 'px';
-    } else {
-       
-        header.classList.remove("fixed");
-        
-        mainContainer.style.marginTop = '0';
-    }
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("fixed");
+    mainContainer.style.marginTop = header.offsetHeight + "px";
+  } else {
+    header.classList.remove("fixed");
+    mainContainer.style.marginTop = "0";
+  }
 });
